9c2e128e958c86417af8acc95eb1656173327ebe
Gary Brodie <Gary.Brodie@qenos.com>
2021-03-01 11:01:13 +1100

    Feb2020V2

---
+++ DataDictionary/Models/.vshistory/Application.cs/2021-01-20_09_30_05_272.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataDictionary.Models
+{
+    public class Application
+    {
+        public int ApplicationId { get; set; }
+
+        [Display(Name = "Application Name")]
+        [Required(ErrorMessage = "Please enter an Application Name")]
+        public string ApplicationName { get; set; }
+
+        [Required(ErrorMessage = "Please enter a Description")]
+        public string Description { get; set; }
+
+        public string Owner { get; set; }
+
+        [Display(Name = "Secondary Contact")]
+        public string SecAppExpert { get; set; }
+
+        [Display(Name = "IS Contact")]
+        public string ISContact { get; set; }
+
+        public string FileName { get; set; }
+
+        public ICollection<KeywordDefinition> KeywordDefinitions { get; set; }
+    }
+}
